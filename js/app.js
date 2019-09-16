0980eaf3ce70c7f2e89888f58b6fc746dcc240e8
Rakesh Mithun <slipknut@gmail.com>
2019-09-17 07:57:46 +1000

    17092019_757am
    Card deck added as a variable array

---
+++ js/app.js
@@ -0,0 +1,94 @@
+//Create an array of cards
+let cards = ['fa fa-diamond', 'fa fa-diamond',
+    'fa fa-paper-plane-o', 'fa fa-paper-plane-o',
+    'fa fa-anchor', 'fa fa-anchor',
+    'fa fa-bolt', 'fa fa-bolt',
+    'fa fa-cube', 'fa fa-cube',
+    'fa fa-anchor', 'fa fa-anchor',
+    'fa fa-leaf', 'fa fa-leaf',
+    'fa fa-bicycle', 'fa fa-bicycle'
+];
+let card = [...cards];
+
+function generatedCard(card) {
+    return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
+}
+
+//Select the deck of cards
+const deck = document.getElementById("deck");
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+function initGame() {
+    let deck = document.querySelector('.deck');
+    let cardHTML = shuffle(cards).map(function(card) {
+        return generatedCard(card);
+    });
+
+    deck.innerHTML = cardHTML.join('');
+}
+
+initGame();
+
+let allCards = document.querySelectorAll('.card');
+let openCards = [];
+
+allCards.forEach(function(card) {
+    card.addEventListener('click', function(e) {
+
+        if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match'));
+        openCards.push(card);
+        card.classList.add('open', 'show');
+
+        if (openCards.length == 2) {
+            if (openCards[0].dataset.card == openCards[1].dataset.card) {
+                openCards[0].classList.add('match');
+                openCards[0].classList.add('open');
+                openCards[0].classList.add('show');
+
+                openCards[1].classList.add('match');
+                openCards[1].classList.add('open');
+                openCards[1].classList.add('show');
+            }
+            setTimeout(function() {
+                openCards.forEach(function(card) {
+                    card.classList.remove('open', 'show');
+                });
+                openCards = [];
+            }, 1000);
+        }
+
+    });
+});
